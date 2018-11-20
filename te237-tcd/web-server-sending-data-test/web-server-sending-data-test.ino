4c86d2c8d5a5348dc18b18016b84ab08c2b40acd
Thiago <ti04@hyperionltda.com.br>
2018-11-20 03:26:12 -0200

    Organização de código, nenhuma alteração

--- te237-tcd/web-server-sending-data-test/web-server-sending-data-test.ino
+++ te237-tcd/web-server-sending-data-test/web-server-sending-data-test.ino
@@ -79,50 +79,40 @@ void atualizaCliente(WiFiClient client, int contagem)
           // if the current line is blank, you got two newline characters in a row.
           // that's the end of the client HTTP request, so send a response:
           if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-//            client.println("HTTP/1.1 200 OK");
-//            client.println("Content-type:text/html");
-//            client.println();
-            // the content of the HTTP response follows the header:
-            String shtml;
-            shtml +="Clique <a href=\"/C\">aqui</a> ver contator.<br>";
-            client.print(shtml);
-//            client.print("Clique <a href=\"/C\">aqui</a> ver contator.<br>");
-
-            // The HTTP response ends with another blank line:
+            String sHTML;
+            sHTML  = "<!doctype html>";
+            sHTML += "<html>";
+            sHTML += "<head>";
+            sHTML += "</head>";
+            sHTML += "<body>"; 
+            sHTML += "Clique <a href=\"/C\">aqui</a> ver contator.<br>";
+            sHTML += "</body>";
+            sHTML += "</html>"; 
+            client.print(sHTML);
             client.println();
-            // break out of the while loop:
             break;
-          } else {    // if you got a newline, then clear currentLine:
+          } else {
             currentLine = "";
           }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
+        } else if (c != '\r') {
+          currentLine += c;   
         }
 
-        // Check to see if the client request was "GET /H" or "GET /L":
         if (currentLine.endsWith("GET /C")) {
-//            client.println("HTTP/1.1 200 OK");
-//            client.println("Content-type:text/html");
-//            client.println();
-            // the content of the HTTP response follows the header:
             String sHTML;
-            sHTML = "<!doctype html>";
-            sHTML +="<html>";
-            sHTML +="<head>";
-            sHTML +="<meta http-equiv=\"refresh\" content=\"5\">";
-            sHTML +="</head>";
-            sHTML+= "<body>"; 
-            sHTML +="Clique <a href=\"/C\">aqui</a> ver contator.<br>";
-            sHTML +="Contador está em ";
-            sHTML +=contagem;
-            sHTML +="<br>";
-            sHTML+= "</body>";
-            sHTML+= "</html>"; 
+            sHTML  = "<!doctype html>";
+            sHTML += "<html>";
+            sHTML += "<head>";
+            sHTML += "<meta http-equiv=\"refresh\" content=\"1\">";
+            sHTML += "</head>";
+            sHTML += "<body>"; 
+            sHTML += "Clique <a href=\"/C\">aqui</a> ver contator.<br>";
+            sHTML += "Contador está em ";
+            sHTML += contagem;
+            sHTML += "<br>";
+            sHTML += "</body>";
+            sHTML += "</html>"; 
             client.print(sHTML);
-
-            // The HTTP response ends with another blank line:
             client.println();
         }
     }
